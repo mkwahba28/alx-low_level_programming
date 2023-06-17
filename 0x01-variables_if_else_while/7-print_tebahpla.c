ac32f89e2e3795464b89aba9decec7ee7ba64210
mkwahba28 <mkwahba28@gmail.com>
2023-06-17 00:24:59 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+    for (i = 'z'; i >= 'a'; i--)
+    {
+	    putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
