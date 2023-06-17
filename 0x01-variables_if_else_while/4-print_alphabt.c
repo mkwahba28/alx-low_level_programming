0be6154f29fa2f554773e953ab9f8c1e0950a93a
mkwahba28 <mkwahba28@gmail.com>
2023-06-17 00:04:55 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters in lower case except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+			continue;
+		else
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
