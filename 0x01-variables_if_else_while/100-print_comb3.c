89cc182fd5bef1bff7b1e1a89832865e941c3f09
mkwahba28 <mkwahba28@gmail.com>
2023-06-17 00:59:15 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit, digit2;
+
+	for (digit = 0; digit <= 9; ++digit)
+	{
+		for (digit2 = 0; digit2 <= 9; ++digit2)
+		{
+			if (digit == digit2)
+			{
+				continue;
+			}
+			if (digit == 1 && digit2 == 0)
+			{
+				continue;
+			}
+			if (digit > digit2)
+			{
+				continue;
+			}
+			else
+			{
+				putchar(digit + '0');
+				putchar(digit2 + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
