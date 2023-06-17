dca9dba1364eacc0e5ddaf92793460c37b2d2312
mkwahba28 <mkwahba28@gmail.com>
2023-06-17 00:27:10 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	char i;
 
-    for (i = 'z'; i >= 'a'; i--)
-    {
-	    putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
