fe9bc71d91afcb77606987d8f2d19cbfad8c9138
mkwahba28 <mkwahba28@gmail.com>
2023-06-16 23:56:56 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,17 +10,14 @@
 
 int main(void)
 {
-    char ch;
-    char CH;
+	char ch;
+	char CH;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	    putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+		putchar(CH);
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-    }
-    for (CH = 'A'; CH <= 'Z'; CH++)
-    {
-        putchar(CH);
-    }
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
