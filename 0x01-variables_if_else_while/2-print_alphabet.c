61f98b7d98b3f5ba282ad694fff5d2a96af3227c
mkwahba28 <mkwahba28@gmail.com>
2023-06-16 23:25:34 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main function printing alphabet characters
+ */
+int main (void)
+{
+	char alphabets;
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	printf("\n");
+
+	return (0);
