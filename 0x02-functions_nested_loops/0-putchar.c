1ce30dd7589b68b4cec8edc31ee6d158c9771503
mkwahba28 <mkwahba28@gmail.com>
2023-06-20 12:49:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/*
+ * main - Entry Point
+ * Description - Print putchar
+ * Return - always 0 success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+	
