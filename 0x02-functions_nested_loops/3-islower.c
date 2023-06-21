25e9c5a38a3d19b3b6dc9f765ae62767d5465432
mkwahba28 <mkwahba28@gmail.com>
2023-06-21 00:01:55 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *Description: returns 1 if the character is lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
