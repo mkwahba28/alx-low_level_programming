d0036ce17f42afbad10336ccbe205c83ee610aa8
mkwahba28 <mkwahba28@gmail.com>
2023-06-22 14:07:43 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,10 @@ void print_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		else
-			_putchar(i);
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
