1642b669d6047137461ade807f9fc0a00f2da11b
mkwahba28 <mkwahba28@gmail.com>
2023-06-22 06:57:09 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,14 @@
  * _isupper - checks if c is uppercase
  * @c: character to be checked
  *
+ * Description: printer character 
+ * 
  * Return: 1 if uppercase, 0 if not
  */
 
 int _isupper(c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
